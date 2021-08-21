99f29fc675cab0a8cea56a0148381f123bc658de
Prakit Srinon <prakit.srinon@gmail.com>
2021-08-21 14:36:57 +0700

    adding files for github pages

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './search-box.styles.css'
+
+export const SearchBox = ({ placeholder, onSearchChange }) => (
+    <input
+        className='search' 
+        type='search' 
+        placeholder={ placeholder } 
+        onChange={ onSearchChange } 
+    />
+)
+
